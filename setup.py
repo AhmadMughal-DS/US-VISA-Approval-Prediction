52f5e3f64ffd030bdc5552f32247789fb6bfc468
AhmadMughal-DS <ahmad.dstech@gmail.com>
2024-11-12 15:55:10 +0500

    again one section is complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name = "US VISA",
+    version = "0.0.0",
+    author="Ahmad",
+    author_email="ahmadzafar392@gmail.com",
+    packages=find_packages(),
+)
+
