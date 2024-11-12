030e20d77b147684faec2cc41a30f81a40e77ea3
AhmadMughal-DS <ahmad.dstech@gmail.com>
2024-11-13 02:15:13 +0500

    feature engineering is done

--- demo.py
+++ demo.py
@@ -1,4 +1,20 @@
+"""
+# this si demo of logging file in us visa project
 from US_VISA.logger import logging
-
-
 logging.info("Welcome to US VISA project")
+"""
+
+
+
+
+from US_VISA.exceptions import USVISAException
+import sys
+
+
+try:
+    a = 2/0
+except Exception as e:    
+    raise USVISAException(e,)
+
+
+
