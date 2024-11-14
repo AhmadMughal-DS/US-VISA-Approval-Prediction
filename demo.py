e5e66c29c50f006bfbbb85a5ed90e2178363343b
AhmadMughal-DS <ahmad.dstech@gmail.com>
2024-11-14 05:07:55 +0500

    data validation done

--- demo.py
+++ demo.py
@@ -6,5 +6,8 @@ try:
     obj = TrainPipeline()
     obj.run_pipeline()
 except USVISAException as e:
-    print(f"Error: {e}")
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    print(f"Error: {e} occurred in script: {fname} at line: {line_number}")
     sys.exit(1)
